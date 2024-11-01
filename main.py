3b142356e11949d9a57fdab13591c7f4f8fe7c80
Keemss <kemardejesus@gmail.com>
2024-10-31 21:52:43 -0400

    Delete main.py
    it was a copy

--- main.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Young Nigga Name Generator.")
-hood_name = input("What is yo hood name?\n")
-skreet_name = input("What are you known for in the skreets?\n")
-print("Yo Young Nigga Name Is: " + hood_name + " " + skreet_name)
